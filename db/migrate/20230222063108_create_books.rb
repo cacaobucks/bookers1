6873ed89b6199becb1f5784051acfcf2f1b953dc
EC2 Default User <ec2-user@ip-172-31-20-137.ap-northeast-1.compute.internal>
2023-03-06 11:18:36 +0000

    Initial Commit

--- db/migrate/20230222063108_create_books.rb
+++ db/migrate/20230222063108_create_books.rb
@@ -2,10 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :author
-      t.integer :year_published
+      t.string :body
       t.timestamps
-      add_column :books, :body, :text
+      #add_column :books, :body, :text
     end
   end
 end
